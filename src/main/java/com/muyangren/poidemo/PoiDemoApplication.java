59b1f35581e7ba08ba27718f25b2ea31ce4580ee
muyangren <jundun877@126.com>
2023-10-18 18:13:16 +0800

    提交内容： 1、修改内容介绍 2、优化部分代码 3、优化部分输出内容 4、修改word模板文件名以及样式优化、删除无用字段

--- src/main/java/com/muyangren/poidemo/PoiDemoApplication.java
+++ src/main/java/com/muyangren/poidemo/PoiDemoApplication.java
@@ -3,6 +3,9 @@ package com.muyangren.poidemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author muyangren
+ */
 @SpringBootApplication
 public class PoiDemoApplication {
 
--- src/test/java/com/muyangren/poidemo/PoiDemoApplicationTests.java
+++ src/test/java/com/muyangren/poidemo/PoiDemoApplicationTests.java
@@ -8,15 +8,20 @@ import com.muyangren.poidemo.entity.Templates;
 import com.muyangren.poidemo.utils.word.template.TemplateData;
 import com.muyangren.poidemo.utils.word.template.TemplateRowRenderData;
 import com.muyangren.poidemo.utils.word.template.TemplateTableRenderPolicy;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * ps：如果你是刚入门的，建议就别看了，徒增痛苦，告诉leader这需求不行
@@ -28,10 +33,11 @@ class PoiDemoApplicationTests {
      * @throws IOException
      */
     @Test
-    void contextLoads() throws IOException {
+    void contextLoads()  {
+        try {
         // 一、初始化数据
         // 1）初始化模板数据-实际是去数据库查询-自行整合
-        Templates templates = initTemplates();
+          Templates templates = initTemplates();
         // 2）初始化家庭成员信息-实际是去数据库查询-自行整合
         List<FamilyMember> familyMemberList = initFamilyMember();
         // 3）初始化工作情况-实际是去数据库查询-自行整合
@@ -49,26 +55,29 @@ class PoiDemoApplicationTests {
         Configure config = Configure.builder().bind("templateRowRenderData", new TemplateTableRenderPolicy()).build();
 
         //  四、导出
-        ClassPathResource classPathResource = new ClassPathResource("templates" + File.separator + "鼠鼠教你如何实现动态导出.docx");
+        ClassPathResource classPathResource = new ClassPathResource("templates" + File.separator + "template.docx");
         XWPFTemplate template = XWPFTemplate.compile(classPathResource.getInputStream(),config).render(
                 templateData);
-
+        Assertions.assertNotNull(template);
         // 通过浏览器下载自行整合下即可
         // controller获取HttpServerResponse
-        template.writeAndClose(new FileOutputStream("C:\\Users\\guangsheng\\Desktop\\output.docx"));
+            template.writeAndClose(Files.newOutputStream(Paths.get("C:\\Users\\WUDI\\Desktop\\export-word.docx")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
     private Templates initTemplates() {
         // 填充一些基本信息
         Templates templates = new Templates();
-        templates.setName("吃了没");
-        templates.setAliases("吃我一拳");
-        templates.setDeptName("勿入鼠穴");
-        templates.setSexName("gay");
+        templates.setName("牧羊人OVO");
+        templates.setAliases("牧羊人OVo");
+        templates.setDeptName("青龙小组");
+        templates.setSexName("未知");
         templates.setPeoples("汉族");
         templates.setBirth("2000");
-        templates.setCulture("胎教");
+        templates.setCulture("小学");
         return templates;
     }
 
@@ -80,9 +89,9 @@ class PoiDemoApplicationTests {
         List<FamilyMember> familyMemberList = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
             FamilyMember familyMember = new FamilyMember();
-            familyMember.setRelationship("好兄弟" + i);
+            familyMember.setRelationship("关系" + i);
             familyMember.setName("姓名" + i);
-            familyMember.setPosition("摸鱼关系户" + i);
+            familyMember.setPosition("职位" + i);
             familyMemberList.add(familyMember);
         }
         return familyMemberList;
@@ -97,9 +106,9 @@ class PoiDemoApplicationTests {
         List<Going> goingList = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
             Going going = new Going();
-            going.setCompany("比命苦美式" + i);
-            going.setAddress("来一杯比我命还苦的冰美式、不加糖" + i);
-            going.setPhone("程序跑不起来，美式，我能跑就行" + i);
+            going.setCompany("工作单位" + i);
+            going.setAddress("地址" + i);
+            going.setPhone("程联系电话" + i);
             goingList.add(going);
         }
         return goingList;
